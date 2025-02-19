268fb2042bcc1eb5b22531e1d9ef435f9c9f17b7
PKyes44 <rugate23@gmail.com>
2025-02-19 19:14:26 +0900

    add svgs

--- src/components/palette/palettes/DrawPalette.tsx
+++ src/components/palette/palettes/DrawPalette.tsx
@@ -69,7 +69,10 @@ function DrawPalette() {
         }`}
         onClick={() => toggleIsDrawing(BLUE_PEN)}
       >
-        <img src="/src/assets/pens/blue-pen.img.svg" alt="" />
+        <img
+          src="https://wbqryjgwbnmvgjsgksvg.supabase.co/storage/v1/object/public/images/pens/blue-pen.img.svg"
+          alt=""
+        />
       </button>
       <button
         className={`${
@@ -77,7 +80,10 @@ function DrawPalette() {
         }`}
         onClick={() => toggleIsDrawing(RED_PEN)}
       >
-        <img src="/src/assets/pens/red-pen.img.svg" alt="" />
+        <img
+          src="https://wbqryjgwbnmvgjsgksvg.supabase.co/storage/v1/object/public/images/pens/red-pen.img.svg"
+          alt=""
+        />
       </button>
       <button
         className={`${
@@ -87,7 +93,10 @@ function DrawPalette() {
         }`}
         onClick={() => toggleIsDrawing(YELLOW_PEN)}
       >
-        <img src="/src/assets/pens/yellow-pen.img.svg" alt="" />
+        <img
+          src="https://wbqryjgwbnmvgjsgksvg.supabase.co/storage/v1/object/public/images/pens/yellow-pen.img.svg"
+          alt=""
+        />
       </button>
       <div className="flex flex-col mt-10">
         <label htmlFor="stroke-weight" className="font-bold">
